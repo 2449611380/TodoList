6034797219bb920ddd234cb5016909ae94de9b9d
Yiwen <2449611380@qq.com>
2023-04-21 13:17:02 +0800

    feat: 创建项目基本结构

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+export class App extends React.Component{
+  render(){
+    return(
+      <div className="App">
+        <img src="./logo.png" alt="logo" className="Logo" />
+
+        <div>
+          
+        </div>
+      </div>
+    )
+  }
+}
