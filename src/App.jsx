a5365266afe9a5e767f3e2cec8f4bad674c0330d
Yiwen <2449611380@qq.com>
2023-04-22 15:05:24 +0800

    feat: 完成事项添加功能，删除模块功能

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,13 @@ import "./App.css";
 export class App extends React.Component{
   constructor(){
     super();
-    
+
+    // 默认获取全部数据 
     axios.get('http://localhost/api/all')
     .then(res => {
-      console.log(res);
+      this.setState({
+        data: res.data
+      })
     })
   }
   state = {
@@ -24,11 +27,37 @@ export class App extends React.Component{
         <img src="./logo.png" className="Logo" alt="logo" />
 
         <div className="center">
-          <AddTodos />
-      
-          <Tabulation />
+          <AddTodos AddTodos={this.AddTodos} data={this.state.data} />
+          <Tabulation data={this.state.data} DelTodos={this.DelTodos} />
         </div>
       </div>
     )
+  }
+  AddTodos = (content, dateTime, grouping) =>{
+    if(content === undefined && dateTime === undefined && grouping === undefined) return;
+    
+    const todos = [...this.state.data, {content: content, date:dateTime, grouping: grouping}];
+    
+    this.setState({
+      data: todos
+    })
+    
+    axios.post("http://localhost/api/add", {
+      content:content,
+      date:dateTime,
+      grouping:grouping,
+    });
+  }
+  DelTodos = (index, id) => {
+    const todos = [...this.state.data];
+    todos.splice(index, 1);
+
+    this.setState({
+      data: todos
+    })
+
+    axios.post("http://localhost/api/del", {
+      id:id,
+    });
   }
 }
--- src/Component/addTodos/addTodos.jsx
+++ src/Component/addTodos/addTodos.jsx
@@ -2,25 +2,66 @@ import React from "react";
 import "./addTodos.css"
 
 export class AddTodos extends React.Component{
-//   constructor(){
-    // super();
-//   }
-  render(){
-    return (
-        <div className="input_block">
-            <div className="INPUT">
-                <input name="content" placeholder="输入你的待办事项" type="text" />
-                <input name="dateTime" type="datetime-local" />
-                <button>添加事项</button>
-            </div>
+    constructor(props){
+        super(props);
+        console.log('this is addTodos');
+        this.props.AddTodos();
+        
+    }
+    state = {
+        content:'',
+        dateTime:'',
+        grouping:'默认分组',
+    }
+    handleChange = (e) => {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
 
-            <div className="grouping">
-                选择分组：
-                <select name="grouping">
-                    <option value="默认分组">默认分组</option>
-                </select>
-            </div>
-      </div>
-    )
-  }
+        switch(name){
+            case "content" :
+                this.setState({
+                    content: value
+                })
+            break
+
+            case "dateTime" :
+                this.setState({
+                    dateTime: value
+                })
+            break
+
+            case "grouping" :
+                this.setState({
+                    grouping: value
+                })
+            break
+
+            default:
+                console.log('default event');
+            break
+        }
+    }
+    addTodos = () => {
+        if(this.state.content === '' || this.state.dateTime === '' || this.state.grouping === '') return alert('请输入待办事项，待办时间，和分组');
+        this.props.AddTodos(this.state.content, this.state.dateTime, this.state.grouping);
+    }
+    render(){
+        return (
+            <div className="input_block">
+                <div className="INPUT">
+                    <input name="content" onChange={this.handleChange} placeholder="输入你的待办事项" type="text" />
+                    <input name="dateTime" onChange={this.handleChange} type="datetime-local" />
+                    <button onClick={this.addTodos}>添加事项</button>
+                </div>
+
+                <div className="grouping">
+                    选择分组：
+                    <select name="grouping" onClick={this.handleChange}>
+                        <option value="默认分组">默认分组</option>
+                    </select>
+                </div>
+        </div>
+        )
+    }
 }
